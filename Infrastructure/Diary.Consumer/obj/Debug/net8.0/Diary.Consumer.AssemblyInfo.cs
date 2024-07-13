f564ef0cf608039812fbe3869e29bc570011eddf
svyatmtk <jesusvoskres0@gmail.com>
2024-07-14 02:31:16 +0300

    Add Grafana Prometheus and Redis

--- Infrastructure/Diary.Consumer/obj/Debug/net8.0/Diary.Consumer.AssemblyInfo.cs
+++ Infrastructure/Diary.Consumer/obj/Debug/net8.0/Diary.Consumer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Diary.Consumer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1334002518e969ffd500d4e33ce526616488fa7a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2db912de4b7b1562f863b0a07a804fd1a550e402")]
 [assembly: System.Reflection.AssemblyProductAttribute("Diary.Consumer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Diary.Consumer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
