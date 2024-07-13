e56b73170624ecd56059a6c1bcc00af211b21322
svyatmtk <jesusvoskres0@gmail.com>
2024-07-14 02:16:45 +0300

    Added Redis, Prometheus and Grapahna

--- Presentation/Diary.Api/obj/Debug/net8.0/Diary.Api.AssemblyInfo.cs
+++ Presentation/Diary.Api/obj/Debug/net8.0/Diary.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Diary.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1334002518e969ffd500d4e33ce526616488fa7a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Diary.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Diary.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
